8a593b76dc2ecc1be1a5de91fe1b08a4b0938588
Gustav Neustadt <gustav@einfach-neu.de>
2022-01-09 21:19:05 +0100

    interim commit

--- src/App.svelte
+++ src/App.svelte
@@ -5,145 +5,121 @@
 	let data = []
 	let labels = []
 	
+	let pollutantsData = []
+	
+	let currentTemporalContextKey = 0
+	let currentTemporalContext
+		
 	const width = 900
 	const height = 700
 	
+	var currentYear = 0
+	let pollutantPadding = 10
+	
+	const pollutants = ["BC", "CO", "NH3", "NMVOC", "NOx", "PM 10", "PM 2.5", "SO2", "TSP"]
+	const years = [1990, 1995, 2000, 2005, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019]
+	const sources = ["Energy", "Industry", "Agriculture", "Waste"]
+	
+
+
 	onMount(async () => {
-		data = await d3.csv("./DataCorrelation.csv").then(d => {
+		data = await d3.csv("./summedDataFrame.csv").then(d => {
 			return d
 		})
-		
-		labels = data.map(obj => Object.values(obj)).map(arr => arr[0])
-		
-		data = data.map(obj => Object.values(obj)).map(arr => arr.splice(1)).map(arr => arr.map(val => parseFloat(val) || 0))
-	})
 
-
-	let rotateX = 0
-	let rotateY = 0
-	let rotate = Math.PI * 1.5
+		pollutantsData = pollutants.map(name => {
+			let pollutantData = data.filter(d => name == Object.values(d)[0])
+			return years.map(year => {
+				let yearData = pollutantData.find(d => d.valueColumn == year)
+				return sources.map(s => parseFloat(yearData[s]))
+			})
+		})
 	
-	let textReverseAngle = Math.PI/2
+	})
+	
+	$: getPollutantSizes = () => {
+		return pollutants.map((pollutant, i) => {
+			let sum = getArraySum(pollutantsData[i][currentYear])
+			return Math.sqrt(sum / Math.PI)
+		})
+	}
+	
+	let getArraySum = (array = []) => {
+		return array.reduce((a, b) => a + b, 0.0)
+	}
+	
+	let sliceOfArray = (start, number, array) => {
+		return array.filter((item, index) => index >= start && index < number + start)
+	}
+	
+	let getCummulativeSize = (array) => {
+		return array.reduce((acc, curr, i) => {
+			let cummulativeValue = acc[acc.length-1] ?? 0.0
+			
+			acc.push(cummulativeValue + curr + array[i+1])
+			return acc
+		}, [0.0])
+	}
+	
+	$: getPollutantPosition = () => {
+		let sizes = getPollutantSizes()
+		let cummulativeSizes = getCummulativeSize(sizes)
+		console.log(sizes, cummulativeSizes)
+		return pollutants.map((pollutant, i) => {
+			let sum = getArraySum(pollutantsData[i][currentYear])
+			return {
+				x: cummulativeSizes[i] + pollutantPadding * i,
+				y: 0
+			}
+		})
+	}
+	
+	
+	// $: console.log(pollutantsData)acc
+	// $: currentTemporalContext = data[currentTemporalContextKey]
+	// $: console.log(currentTemporalContext)
 	
 	const radius = 200
-
-	let textLabels = []
-	
-	let textMargin = 10
 	
 	const ribbonGenerator = d3.ribbon().radius(radius);
 	
-	$: chords = d3.chord()
-		.sortSubgroups(d3.descending)
-		.sortChords(d3.descending)
-		.padAngle(0.05)(data)
+	// $: chords = d3.chord()
+	// 	.sortSubgroups(d3.descending)
+	// 	.sortChords(d3.descending)
+	// 	.padAngle(0.05)(data)
 		
-	$: groups = chords.groups.map((group, index) => {
-		group.label = labels[group.index]
-		group.startAngle = group.startAngle
-		group.endAngle = group.endAngle
-		group.labelPosition = (calcLabelPos(group.startAngle, group.endAngle) + rotate)  % (Math.PI * 2)
-
-		
-		group.textLabelReverse = group.labelPosition > Math.PI ? true : false
-		
-				
-		group.textLabel = textLabels[index]
-		group.textLabelLength = 0
-		
-		if(group.textLabel) {
-			group.textLabelLength = Math.max(...[...group.textLabel.children].map(el => el.textLength.baseVal.value))
-		}
-		
-		group.valueTextLabel = new Intl.NumberFormat("en-US", {
-			maximumSignificantDigits: 2
-		}).format(group.value)+" kt"
-		
-		// console.log(group.textLabel ? [group.textLabel.textLength, group.textLabel] : null)
-		
-		return group
-	})
-	
-	
-	$: yScale = d3.scaleBand()
-		.domain(data.map(d => d["Source"]))
-		.range([0, height])
-		
-	$: xScale = d3.scaleLinear()
-		.domain([0, d3.max(data, d => parseFloat(d["2019"]))])
-		.range([0, width])
-	
-	function calcLabelPos(startAngle, endAngle) {
-		let sumAngle = endAngle - startAngle
-		return startAngle + sumAngle / 2
-	}
-	
-	function Rad2Deg(radians) {
-	  var pi = Math.PI;
-	  return radians * (180/pi);
-	}
-			  
 </script>
 
 <style>
-	path {
-		fill: black;
-		opacity: 0.3
-	}
-	
-	path:hover {
-		opacity: 1
-	}
-	
-	text {
-		font-family: "SF Mono"
-	}
-	
-	.title {
-		font-weight: bold;
-	}
-	.value {
-		fill: #aaa;
-	}
+circle {
+	opacity: 0.3;
+}
 </style>
 
 <h1>DEMO</h1>
 
-<input type="range" bind:value="{textMargin}" min="-500" max="500">
-<input type="range" bind:value="{rotateY}" min="-500" max="500"> 
-<input type="range" bind:value="{rotate}" step="0.01" min="0" max="10">
+
+<input type="range" min="0" max="{years.length-1}" bind:value="{currentYear}">
+
 <pre>
-textMargin: {textMargin}
+	{years[currentYear]}
+<!-- textMargin: {textMargin}
 rotateX: {rotateX}
 rotateY: {rotateY}
 rotate: {rotate} rad
-	
+	 -->
 </pre>
 
-<svg {width} {height}>
 
-		<g transform="translate({width/2} {height/2}) rotate({-90} {rotateX} {rotateY})">
-			{#each groups as group, i}
-<!-- 			rotate({Rad2Deg(group.labelPosition)} -{radius} 0)  -->
-				<text 
-					bind:this="{textLabels[i]}"
-					text-anchor="{group.textLabelReverse ? 'end' : ''}" 
-					transform="
-						rotate({Rad2Deg(group.labelPosition)} 0 0) 
-						translate({textMargin + radius})
-						{group.textLabelReverse ? 'scale(-1 -1)' : ''}
-					"
-					>
-					<tspan class="title" x="0" y="0">{group.label}</tspan><tspan class="value" x="0" y="19">{group.valueTextLabel}</tspan>
-				</text>
+{#if pollutantsData.length > 0}
+	<svg {width} {height}>
+		<g transform="translate(100, 100)">
+			{#each pollutants as name, i}
+				<g transform="translate({getPollutantPosition()[i].x}, {getPollutantPosition()[i].y})">
+					<circle cx="0" cy="0" r="{getPollutantSizes()[i]}" fill="grey"></circle>
+					<text>{name}</text>
+				</g>
 			{/each}
 		</g>
-		
-		<g transform="translate({width/2} {height/2}) rotate({Rad2Deg(rotate)} 0 0)" >
-			{#each chords as entry, i}
-				<path d="{ribbonGenerator(entry)}">
-					
-				</path>
-			{/each}
-		</g>
-</svg>
+	</svg>
+{/if}
