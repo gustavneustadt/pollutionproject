832b3c7e2f7c96c913639e04e24c67b36fb2df84
Gustav Neustadt <gustav@einfach-neu.de>
2022-01-10 11:23:38 +0100

    bubbles workingn

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,8 @@
 <script>
 	import * as d3 from 'd3'
+	import chroma from "chroma-js"
 	import { onMount } from 'svelte'
+	import PollutantBubble from './PollutantBubble.svelte'
 	
 	let data = []
 	let labels = []
@@ -10,16 +12,37 @@
 	let currentTemporalContextKey = 0
 	let currentTemporalContext
 		
-	const width = 900
+	const width = 1000
 	const height = 700
 	
 	var currentYear = 0
 	let pollutantPadding = 10
 	
-	const pollutants = ["BC", "CO", "NH3", "NMVOC", "NOx", "PM 10", "PM 2.5", "SO2", "TSP"]
+	const maxPollutantSize = 6000
+	const minPollutantSize = 100
+	
+	const pollutants = 		
+	["BC",		"CO", 		"NH3", 		"NMVOC", 	"NOx",		"PM 10", 	"PM 2.5",	"SO2",		"TSP"]
+	const pollutantColors = 
+	["#3B90ED", "#FFE789", 	"#37D2AC", 	"#E8574F", 	"#34B1EA",	"#6C6BCC", 	"#66C27D",	"#AA64CE",	"#E19F41"]
 	const years = [1990, 1995, 2000, 2005, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019]
 	const sources = ["Energy", "Industry", "Agriculture", "Waste"]
 	
+	
+	function getPollutantTextColor(pollutantIndex) {
+		let color = chroma(pollutantColors[pollutantIndex])
+		return color.desaturate(1).luminance(0.3)
+	}
+	
+	function getPollutantBorderColor(pollutantIndex) {
+		let color = chroma(pollutantColors[pollutantIndex])
+		return color.desaturate(0.3).luminance(0.7)
+	}
+	
+	function getPollutantBgColor(pollutantIndex) {
+		let color = chroma(pollutantColors[pollutantIndex])
+		return color.desaturate(0.3).luminance(0.8)
+	}
 
 
 	onMount(async () => {
@@ -37,40 +60,133 @@
 	
 	})
 	
-	$: getPollutantSizes = () => {
-		return pollutants.map((pollutant, i) => {
-			let sum = getArraySum(pollutantsData[i][currentYear])
-			return Math.sqrt(sum / Math.PI)
-		})
+	$: getMaxValue = () => {
+		if(pollutantsData.length < 1) {
+			return 0
+		}
+		
+		return Math.max(...pollutants.map(
+			(pollutant, i) => 
+				Math.max(...pollutantsData[i].map(year => getArraySum(year))
+				
+			)
+		))
 	}
 	
-	let getArraySum = (array = []) => {
+	$: getMinValue = () => {
+		if(pollutantsData.length < 1) {
+			return 0
+		}
+		
+		return Math.min(...pollutants.map(
+			(pollutant, i) => 
+				Math.min(
+					...pollutantsData[i].map(year => 
+						getArraySum(year)
+					).filter(x => x > 0)
+				
+				)
+			)
+		)
+	}
+	
+	$: getPollutantValue = (pollutantIndex, year = null, yearShift = null) => {
+		if(pollutantsData.length < 1) {
+			return 0.0
+		}
+		
+		year = year ?? currentYear
+		let value = getArraySum(pollutantsData[pollutantIndex][year+yearShift])
+		
+		return value
+	}
+	
+	
+	
+	function getPollutantSize(pollutantIndex) {
+		// let pollutantValue = getPollutantValue(pollutantIndex)
+		let sizeValue = getPollutantPercentageChangeOverAll(pollutantIndex)* (maxPollutantSize - minPollutantSize) + minPollutantSize
+		
+		return Math.sqrt(sizeValue / Math.PI)
+	}
+
+	function getFirstPollutantValue(pollutantIndex) {
+		for(let i = 0; i < years.length -1; i++) {
+			let value = getPollutantValue(pollutantIndex, i)
+			if(value > 0) {
+				return value
+			}
+		}
+	}
+	
+	function getPollutantPercentageChangeOverAll(pollutantIndex) {
+		let firstPollutantValue = getFirstPollutantValue(pollutantIndex)
+		let currentPollutantValue = getPollutantValue(pollutantIndex)
+		
+		return currentPollutantValue/firstPollutantValue
+	}
+	
+	function getPollutantPercentageChange(pollutantIndex) {
+		let prevPollutantValue = getPollutantValue(pollutantIndex, null, -1)
+		let currentPollutantValue = getPollutantValue(pollutantIndex)
+		let value = (currentPollutantValue/prevPollutantValue)
+		
+		return isFinite(value) ? value : 1.0
+	}
+	
+	// let scaleY = d3.scaleLog().
+	
+	$: xScale = d3.scaleLog().domain([getMinValue(), getMaxValue()]).range([10, 700])
+	$: yScale = d3.scaleLog().domain([1, 1.2]).range([0.0, 100.0])
+	
+	function getPollutantPosition(pollutantIndex, xScale, yScale) {
+		let x = getPollutantValue(pollutantIndex)
+		let y = getPollutantPercentageChange(pollutantIndex)
+		return {
+			x: xScale(x),
+			// y: yScale(getPollutantPercentageChange(pollutantIndex))
+			y: yScale(y)* -1
+		}
+	}
+	
+	
+	function getArraySum(array = []) {
 		return array.reduce((a, b) => a + b, 0.0)
 	}
 	
-	let sliceOfArray = (start, number, array) => {
+	function sliceOfArray(start, number, array) {
 		return array.filter((item, index) => index >= start && index < number + start)
 	}
 	
-	let getCummulativeSize = (array) => {
-		return array.reduce((acc, curr, i) => {
-			let cummulativeValue = acc[acc.length-1] ?? 0.0
-			
-			acc.push(cummulativeValue + curr + array[i+1])
-			return acc
-		}, [0.0])
+	function getPercantageString(percentage) {
+		return ((percentage - 1.0) * 100.0).toFixed(0) + " %"
 	}
 	
-	$: getPollutantPosition = () => {
-		let sizes = getPollutantSizes()
-		let cummulativeSizes = getCummulativeSize(sizes)
-		console.log(sizes, cummulativeSizes)
-		return pollutants.map((pollutant, i) => {
-			let sum = getArraySum(pollutantsData[i][currentYear])
+	function getPollutantNamePosition(pollutantIndex) {
+		let size = getPollutantSize(pollutantIndex)
+		let yTextOffset = 6
+		if(size < 35) {
 			return {
-				x: cummulativeSizes[i] + pollutantPadding * i,
-				y: 0
+				x: 0,
+				y: -size - 10
 			}
+		}
+		return {
+			x: 0,
+			y: yTextOffset
+		}
+	}
+	
+	$: getOrderedPollutants = () => {
+		let mapped = pollutants.map((name, i) => {
+			return {
+				name: name,
+				i: i
+			}
+		})
+		let sizes = mapped.map(pollutant => getPollutantValue(pollutant.i))
+		return mapped.sort((a, b) => {
+			return sizes[a.i] > sizes[b.i]
 		})
 	}
 	
@@ -92,7 +208,22 @@
 
 <style>
 circle {
-	opacity: 0.3;
+	fill: var(--bgColor);
+	stroke: var(--borderColor);
+	stroke-width: 4px;
+}
+text {
+	font-family: "Merriweather Sans";
+	font-size: 1rem;
+	letter-spacing: 0.1rem;
+	fill: var(--textColor);
+}
+
+.pollutant--name {
+	font-weight: 400;
+}
+.pollutant--percentage {
+	font-weight: 300;
 }
 </style>
 
@@ -103,6 +234,7 @@ circle {
 
 <pre>
 	{years[currentYear]}
+	{getOrderedPollutants()}
 <!-- textMargin: {textMargin}
 rotateX: {rotateX}
 rotateY: {rotateY}
@@ -114,12 +246,22 @@ rotate: {rotate} rad
 {#if pollutantsData.length > 0}
 	<svg {width} {height}>
 		<g transform="translate(100, 100)">
-			{#each pollutants as name, i}
-				<g transform="translate({getPollutantPosition()[i].x}, {getPollutantPosition()[i].y})">
-					<circle cx="0" cy="0" r="{getPollutantSizes()[i]}" fill="grey"></circle>
-					<text>{name}</text>
+		{#each getOrderedPollutants() as pollutant, i}
+			{#if getPollutantValue(pollutant.i) > 0}
+				<g transform="translate({getPollutantPosition(pollutant.i, xScale, yScale).x}, {getPollutantPosition(pollutant.i, xScale, yScale).y})" style="
+					--borderColor: {getPollutantBorderColor(pollutant.i)};
+					--textColor: {getPollutantTextColor(pollutant.i)};
+					--bgColor: {getPollutantBgColor(pollutant.i)};
+				">
+					<circle cx="0" cy="0" r="{getPollutantSize(pollutant.i)}" fill="grey"></circle>
+					<text y={getPollutantNamePosition(pollutant.i).y} x={getPollutantNamePosition(pollutant.i).x} class="pollutant--name" text-anchor="middle">{pollutant.name}</text>
+					<text y="{getPollutantSize(pollutant.i) + 20}" x="" class="pollutant--percentage" text-anchor="middle">{
+						getPercantageString(getPollutantPercentageChange(pollutant.i))
+						
+						}</text>
 				</g>
-			{/each}
-		</g>
-	</svg>
+			{/if}
+		{/each}
+	</g>
+</svg>
 {/if}
---
+++ src/PollutantBubble.svelte
@@ -0,0 +1,46 @@
+<script>
+	export let pollutant = {
+		pos: {
+			x: 0,
+			y: 0
+		},
+		name: "JUST",
+		percentageChange: "100 %",
+		size: 20
+	}
+</script>
+
+<style>
+	circle {
+		fill: var(--bgColor);
+		stroke: var(--borderColor);
+		stroke-width: 4px;
+	}
+	text {
+		font-family: "Merriweather Sans";
+		font-size: 1rem;
+		letter-spacing: 0.1rem;
+		fill: var(--textColor);
+	}
+	
+	.pollutant--name {
+		font-weight: 400;
+	}
+	.pollutant--percentage {
+		font-weight: 300;
+	}
+</style>
+
+<g transform="translate({pollutant.pos.x}, {pollutant.pos.y})" style="
+	--borderColor: grey;
+	--textColor: darkgrey;
+	--bgColor: lightgrey;
+">
+	<circle cx="0" cy="0" r="{pollutant.size}" fill="grey"></circle>
+	<text y={pollutant.text.pos.y} x={pollutant.text.pos.x} class="pollutant--name" text-anchor="middle">{pollutant.name}</text>
+	<text y="{pollutant.size + 20}" x="" class="pollutant--percentage" text-anchor="middle">
+		{
+		pollutant.percentageChange
+		}
+	</text>
+</g>
