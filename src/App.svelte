f2849de28ae39a0721b97bcb673c8e920ee37c17
Gustav Neustadt <gustav@einfach-neu.de>
2022-03-26 14:41:37 +0100

    componized project
    Finished first graph

--- src/App.svelte
+++ src/App.svelte
@@ -12,6 +12,8 @@
 	import { tweened } from 'svelte/motion';
 	import { cubicOut } from 'svelte/easing';
 	
+	import PollutantGraph from "./PollutantGraph.svelte"
+	
 	let data = []
 	
 	let pollutantsData = []
@@ -30,6 +32,7 @@
 		subSources: null,
 		subSourceDescriptions: null,
 		subSourcesPerSourceGroup: null,
+		pollutants: null,
 		years: [1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019],
 		getMinYear: function() {
 			return Math.min(...this.years)	
@@ -37,7 +40,7 @@
 		getMaxYear: function() {
 			return Math.max(...this.years)	
 		},
-		pollutants: ["BC",		"CO", 		"NH3", 		"NMVOC", 	"NOx",		"PM10", 	"PM2.5",	"SO2",		"TSP"],
+		pollutantNames: ["BC",		"CO", 		"NH3", 		"NMVOC", 	"NOx",		"PM10", 	"PM2.5",	"SO2",		"TSP"],
 		getYear: function(yearIndex) {
 			return this.years[yearIndex]	
 		},
@@ -74,39 +77,9 @@
 	
 	setContext("store", store)
 	
-	const width = 800
-	const height = 1000
-	
 	let initialized = false
 	
 	var currentYear = 0
-	
-	const pollutants = $store.pollutants
-	const pollutantColors = 
-	["#509CF7", "#FFCC00", 	"#5856D6", 	"#34C759", 	"#FF2D55",	"#AF52DE", 	"#FF9500",	"#37D2AC",	"#83DCFF"]
-	const years = $store.years
-	const sources = ["Energy", "Industry", "Agriculture", "Waste"]
-	
-	let trendLineValues = pollutants.map(_ => {
-		return 0;
-	})
-	
-	let pollutantPositions = pollutants.map(_ => {
-		return {
-			x: 0,
-			y: 0
-		}
-	})
-	
-	$: cleanedTrendValues = trendLineValues.filter(val => Math.abs(val) != Infinity)
-	let meanTrend = tweened(0, {
-		duration: 600,
-		easing: cubicOut
-	})
-	
-	$: {
-		meanTrend.set(cleanedTrendValues.reduce((acc, curr) => acc + curr)/cleanedTrendValues.length)
-	}
 
 	async function init() {
 		let reshapeGroupedItems = (groupedItemsArray) => {
@@ -114,7 +87,7 @@
 				let yearData = sourceGroupYears.map(([year, data]) => {
 					let reshapedData = Object.fromEntries(
 						Object.entries(data[0]).filter(
-							([key, _]) => pollutants.includes(key)
+							([key, _]) => $store.pollutantNames.includes(key)
 						).map(
 							([key, value]) => {
 							return [key, parseFloat(value)]
@@ -128,17 +101,13 @@
 		}
 		
 		
-		data = await d3.csv("./pollutantsSum.csv")
-		
-		pollutantsData = pollutants.map(name => {
+		$store.pollutants = await d3.csv("./pollutantsSum.csv").then(d => {
+			let grouped = [...d3.group(d, d => d.pollutant)]
 			
-			let pollutantData = data.filter(d => {
-				return d[""] == name
-			})[0]
-			
-			let sorted = years.map(year => pollutantData[year])
-			
-			return sorted
+			let reshape = grouped.map(([pollutantName, years]) => {
+				return [pollutantName, years.map(year => [year.year, parseFloat(year.amount)])]
+			})
+			return reshape
 		})
 		
 		const fetchSourceGroupsSum = await d3.csv("./sourceGroupsSum.csv")
@@ -170,7 +139,7 @@
 		
 		$store.subSourcesPerSourceGroup = fetchSubSourcesPerSourceGroup
 		
-		return 1
+		return true
 	}
 
 	onMount(async () => {
@@ -179,231 +148,11 @@
 		})
 	})
 	
-	$: getMaxValue = () => {
-		if(pollutantsData.length < 1) {
-			return 0
-		}
-		
-		let pollutantMaxes = pollutantsData.map(pollutant => Math.max(...pollutant))
-		
-		return Math.max(...pollutantMaxes)
-	}
-	
-	$: getMinValue = () => {
-		if(pollutantsData.length < 1) {
-			return 0
-		}
-		
-		let pollutantMaxes = pollutantsData.map(pollutant => Math.min(...pollutant.filter(x => x > 0)))
-				
-		return Math.min(...pollutantMaxes)
-	}
-	
-	$: pollutantValues = pollutants.map((_, i) => {
-		if(pollutantsData.length > 0) {
-			return years.map((_, y) => {
-				return Number(pollutantsData[i][y])
-			})				
-		}
-	})
-
-	$: xScale = d3.scaleLinear().domain([100, 0]).range([0, width - 210])
-	$: yScale = d3.scaleLinear().domain([-63, 10]).range([500, 0])
-	$: sizeScale = d3.scaleSqrt().domain([getMinValue(), getMaxValue()]).range([5, 80])
-	
-	const axisLabels = {
-		x: [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100],
-		y: [10, 0, -10, -20, -30, -40, -50]
-	}
-	
-	$: scales = {
-		x: xScale,
-		y: yScale,
-		size: sizeScale
-	}
-	
-	let maxLabel = {
-		xMin: Math.min(...axisLabels.x),
-		xMax: Math.max(...axisLabels.x),
-		yMin: Math.min(...axisLabels.y),
-		yMax: Math.max(...axisLabels.y),
-	}
-	
-	function getArraySum(array = []) {
-		return array.reduce((a, b) => parseFloat(a) + parseFloat(b), 0.0)
-	}
-	
-   	const niceNumbers = $store.niceNumbers
-
-	
-	$: getOrderedPollutants = () => {
-		let mapped = pollutants.map((name, i) => {
-			return {
-				name: name,
-				i: i
-			}
-		})
-		return mapped
-		// let sizes = mapped.map(pollutant => getPollutantValue(pollutant.i))
-		// return mapped.sort((a, b) => {
-		// 	return sizes[a.i] > sizes[b.i]
-		// })
-	}
-	
-	const radius = 200
-	
-	const ribbonGenerator = d3.ribbon().radius(radius);
-
-	$: pollutantsComponentData = pollutants.map((pollutant, i) => {
-		let values = pollutantValues[i]
-
-		return {
-			scale: {
-				x: xScale,
-				y: yScale,
-				size: sizeScale
-			},
-			year: currentYear,
-			values: values,
-			name: pollutant,
-			color: pollutantColors[i],
-			textColor: "#202020",
-			borderColor: "#101010"
-		}
-	})
-	
-	const linkGenerator = d3.linkVertical()
-		.x(function(d) {
-			return d.x;
-		})
-		.y(function(d) {
-			return d.y;
-		});
-	
-	$: links = sources.map((source, i) => {
-		let target = {
-			x: i * 100,
-			y: 0
-		}
-		return pollutantPositions.map(position => {
-			let data = {
-				target: target,
-				source: position
-			}
-			return linkGenerator(data)
-		})
-	})
-	  
-	function newLink(d) {
-		let [source, target] = [d.source, d.target]
-		
-		let cu = .2
-		
-		let yi = d3.interpolateNumber(d.target.y, d.source.y)
-		
-		return	"M" + source.x + "," + source.y + " " +
-				"C" + source.x + "," + (source.y - yi(cu)) + " " +
-					  target.x + "," + (target.y + yi(cu)) + " " +
-					  target.x + "," + target.y + " " +
-				"L" + (target.x + target.width) + "," + target.y + " " +
-				"C" + (target.x + target.width) + "," + (target.y + yi(cu)) + " " +
-			  		  (source.x + source.width) + "," + (source.y - yi(cu)) + " " +
-			  		  (source.x + source.width) + "," + source.y + " " +
-				"L" + source.x + "," + source.y + " "
-					  
-	}
-	
-	function getConnection (pollutantPosition, sourcePosition) {
-
-		let newLinkData = {
-			source: {
-				x: sourcePosition.x,
-				y: sourcePosition.y,
-				width: 20,
-			},
-			target: {
-				x: pollutantPosition.x,
-				y: pollutantPosition.y,
-				width: 1
-			}
-		}
-		
-		return newLink(newLinkData)
-	}
-	
-	function getSourcePosition(i) {
-		return {
-			x: xScale((maxLabel.xMax / sources.length) * i),
-			y: yScale(maxLabel.yMin) + 100
-		}
-	}
-	
-	function getPollutantConnectionPoint(i, j) {
-		let sourcePosition = getSourcePosition(i)
-		return {
-			x: sourcePosition.x + j * 20,
-			y: sourcePosition.y
-		}
-	}
 </script>
 
 <style>
-* {
-	font-family: "Fira Sans";
-}
-.axis-grid--horizontal, .axis-grid--vertical {
-	stroke: #f4f4f4;
-}
-
-text {
-/* 	fill: #d0d0d0; */
-/* 	font-family: "Merriweather"; */
-	font-weight: 200;
-	font-size: 0.8rem;
-}
-
-.axis-grid--horizontal.zero {
-	stroke: #d9d9d9;
-}
-
-.axis-grid--horizontal.trend {
-	stroke: red;
-	stroke-width: 3px;
-	opacity: .1;
-}
-
-.source__name {
-	font-weight: 500;
-	fill: white;
-}
-
-.source__block {
-	fill: #d9d9d9;
-}
-.connection {
-	fill: black;
-	stroke: none;
-/* 	fill: none; */
-/* 	stroke: #f4f4f4; */
-/* 	stroke-width: 2; */
-	opacity: 0.03;
-}
-
 .flex {
 	display: flex
-}
-
-svg {
-	width: 100%;
-	position: absolute;
-	top: 0;
-	left: 0;
-	
-}
-.graph-wrapper {
-	width: 100%;
-	height: 100rem;
-	position: relative;
 }
 header {
 	position: fixed;
@@ -434,46 +183,7 @@ header {
 	<Header bind:currentYearIndex={currentYear} />
 {/if}
 <div class="flex">
-	<div class="graph-wrapper">
-		
-		{#if initialized}
-			<svg viewBox="0 0 {width} {height}" preserveAspectRatio="xMidYMid meet">
-				<g transform="translate(100, 40)">
-					<g transform="translate(0, {yScale(-50) + 100})">
-						<PollutantSources year={years[currentYear]} xScale={xScale}/>			
-							
-						{#each axisLabels.x as label}
-							<line class="axis-grid--vertical" x1="{xScale(label)}" y1="-70" x2="{xScale(label)}" y2="0"></line>
-						{/each}		
-					</g>
-					
-					<text transform="rotate(-90) translate({yScale(40)}, -70)" text-anchor="middle">Change to previous year</text>
-					<text transform="translate({xScale(50)}, {yScale(maxLabel.yMin) + 60})" text-anchor="middle">Compared to base year</text>
-					
-					<text class="trend" x="{xScale(maxLabel.xMax) + 10}" y="{yScale($meanTrend)}" alignment-baseline="central" text-anchor="left">Trend per year</text>
-					
-					<line class="axis-grid--horizontal trend" x1="{xScale(maxLabel.xMin)}" y1="{yScale($meanTrend)}" x2="{xScale(maxLabel.xMax)}" y2="{yScale($meanTrend)}"></line>
-				{#each axisLabels.y as label}
-					<line class="axis-grid--horizontal {label == 0 ? 'zero' : ''}" x1="{xScale(0)}" y1="{yScale(label)}" x2="{xScale(maxLabel.xMax)}" y2="{yScale(label)}"></line>
-					<text class="axis-label--horizontal" x="-10" y="{yScale(label)}" text-anchor="end" alignment-baseline="central">{niceNumbers(label)} %</text>
-				{/each}
-				{#each axisLabels.x as label}
-					<line class="axis-grid--vertical" x1="{xScale(label)}" y1="{yScale(maxLabel.yMax)}" x2="{xScale(label)}" y2="{yScale(maxLabel.yMin)}"></line>
-					<text class="axis-label--vertical" x="{xScale(label)}" y="{yScale(maxLabel.yMin) + 20}" text-anchor="middle">{niceNumbers(label)} %</text>
-				{/each}
-				
-				{#each getOrderedPollutants() as pollutant, i}
-						<PollutantBubble
-							on:click={() => alert(1)}
-							scales={scales}
-							year={currentYear} 
-							bind:trend={trendLineValues[i]}
-							bind:pollutantPosition={pollutantPositions[i]}
-							pollutant={pollutantsComponentData[i]}
-						></PollutantBubble>
-				{/each}
-			</g>
-		</svg>
-		{/if}
-	</div>
+	{#if initialized}
+		<PollutantGraph currentYearIndex={currentYear}/>
+	{/if}
 </div>
--- src/PollutantBubble.svelte
+++ src/PollutantBubble.svelte
@@ -1,24 +1,27 @@
 <script>
-	export let pollutant 
-	export let year
-	export let scales
 	import chroma from "chroma-js"
-	
+	import * as d3 from "d3"
 	import { tweened } from 'svelte/motion';
 	import { cubicOut } from 'svelte/easing';
 	
 	import { createEventDispatcher } from "svelte";
 	
 	
+	export let pollutant 
+	export let year
+	let scales = pollutant.scale
+	export let activeCluster
 	export let trend
 	
 	export let pollutantPosition
 	
+	
+	let element 
 	const dispatch = createEventDispatcher();
 	
 	const clickEvent = () => {
 		dispatch("click", pollutant.id);
-	  };
+	};
 	
 	$: pollutantPosition = {
 		x: $pos.x,
@@ -29,15 +32,39 @@
 		easing: cubicOut
 	})
 	
-	let size = tweened(scales.size(0), {
+	let size = tweened(null, {
 		duration: 400,
 		easing: cubicOut
 	})
 	
-	let opacity = tweened(0.0, {
+	let opacity = tweened(1, {
 		duration: 400,
 		easing: cubicOut
 	})
+	
+	let lineGenerator = d3.line().curve(d3.curveCardinal.tension(0.5))
+	let lastPos = null
+	$: positionsArray = pollutant.values.map(([mapYear, _], i, array) => {
+		if(year < mapYear) {
+			return [mapYear, [lastPos.x, lastPos.y]]
+		}
+		let pos = calcPosition(mapYear)
+		lastPos = pos
+		return [mapYear, [pos.x, pos.y]]
+	})
+
+	// $: console.log(positionsArray)
+	
+	$: linePathTweened.set(positionsArray)
+	let linePathTweened = tweened(null, {
+		easing: cubicOut
+	})
+	$: linePath = lineGenerator($linePathTweened.filter(([filterYear, _]) => filterYear <= year + 1).map(p => p[1]))
+	// $: console.log($linePathTweened)
+	
+	$: trend = pollutant.values.filter(([filterYear, _]) => filterYear <= year).map(([mapYear, value], _, values) => {
+			return [mapYear, getPercentageChange(values, mapYear, value)]
+		})
 	
 	
 	$: {
@@ -57,25 +84,43 @@
 	}
 	
 	function getFirstYearIndex() {
-		return pollutant.values.findIndex(x => x > 0)
+		let index = pollutant.values.findIndex(([_, amount]) => amount > 0)
+		
+		return index ?? 0
+	}
+	
+	function getFirstYear() {
+		let year = pollutant.values.find(([_, amount]) => amount > 0)
+		
+		return year ? year[0] : null
 	}
 	
 	function getBaseValue(values) {
-		return values.find(x => x > 0)
+		let value = values.find(([_, amount]) => amount > 0)
+		return value ? value[1] : null
 	}
 	
-	function getPrevValue(values, year) {
-		let baseValue = getBaseValue(values)
-		return values[year-1]? values[year-1] : baseValue
+	function getPrevValue(values, needleYear) {	
+		
+		let value = values.find(([year, amount]) => year == needleYear - 1 && amount > 0)
+		return value ? value[1] : getBaseValue(values)
 	}
 	
-	function getCurrentValue(year) {
-		return pollutant.values[year]
+	function getCurrentValue(needleYear) {
+		let value = pollutant.values.find(([year, _]) => year == needleYear)
+		let returnVal = value ? value[1] : null
+		return returnVal
 	}
 	
 	function getPercentageChange(values, currentYear, currentValue) {
 		let prev = getPrevValue(values, currentYear)
-		return ((prev / currentValue) - 1) * -100
+		
+		let val = ((prev / currentValue) - 1) * -100
+		
+		if(Math.abs(val) == Infinity || val == NaN) {
+			return 0
+		}
+		return val
 	}
 	
 	function sliceArray(array, start, number) {
@@ -86,17 +131,20 @@
 		return (percentage * 100).toFixed(0)
 	}
 	
-	$: bgColor = chroma(pollutant.color).luminance(0.86)
-	$: textColor = chroma(pollutant.color).luminance(0.5).desaturate(0.9)
-	$: borderColor = chroma(pollutant.color).luminance(0.79)
+	let color = scales.color(pollutant.name)
+	$: bgColor = chroma(color).luminance(0.86)
+	$: bgColorTransparent = bgColor.alpha(0.5)
+	$: textColor = chroma(color).luminance(0.5).desaturate(0.9)
+	$: borderColor = chroma(color).luminance(0.79)
 	$: baseSizeStrokeColor = borderColor.luminance(0.60)
 	
 	$: currentValue = getCurrentValue(year)
 	
+	
+	
 	function calcPosition(year = -1) {
 		// if the year is 0 or start of the pollutant => change is 0
-		
-		let firstYear = getFirstYearIndex()
+		let firstYear = getFirstYear()
 		
 		if(firstYear >= year) {
 			return {
@@ -124,7 +172,12 @@
 	$: prevValue = getPrevValue(pollutant.values, year)
 	
 	$: percentageChange = getPercentageChange(pollutant.values, year, currentValue)
-	$: trend = getPercentageChange(pollutant.values, year, currentValue)
+	$: trend = trend.map(i => {
+		
+		return i
+	})
+	
+	// $: console.log(trend)
 	
 	$: percentageChangeToBase = currentValue / baseValue
 	
@@ -134,27 +187,40 @@
 		x: 0,
 		y: $size < 30 ? -$size - 10 : 8
 	}
+
+
+	function activate() {
+		if(isActive) {
+			activeCluster = null
+			return
+		}
+		activeCluster = pollutant.name
+		d3.select(element).raise()
+	}
+	$: isActive = activeCluster == pollutant.name
+	$: someActive = activeCluster != null
 	
-	$: valuesScaled = sliceArray(pollutant.values, 0, year).map((value, i) => {
-		let pos = getPos(
-			value,
-		   	getPercentageChange(pollutant.values, i, value),
-			scales.x,
-			scales.y
-		   )
-		   
-		return pos
-	})
 	
-	// console.log(valuesScaled)
+	let linePathLabels = {
+		"BC": [2003, 2009, 2018, 2014],
+		"CO": [1991, 1994, 1998, 2003, 2009, 2019],
+		"NH3": [1991, 2019],
+		"NMVOC": [1991, 1994, 2000, 2003, 2009, 2019],
+		"NOx": [1991, 1994, 2000, 2009, 2019],
+		"PM10": [1996, 2001, 2005, 2009],
+		"PM2.5": [1996, 2001, 2005, 2009, 2016],
+		"SO2": [1991, 1995, 1998, 2009],
+		"TSP": [1991, 1995, 2001, 2009]
+	}
 </script>
 
 <style>
 	circle.size {
-		fill: var(--bgColor);
+		fill: var(--bgColorTransparent);
 		stroke: var(--borderColor);
 		stroke-width: 4px;
-		opacity: 0.5;
+		transition: .3s fill ease;
+		/* opacity: 0.5; */
 /* 		mix-blend-mode: multiply; */
 	}
 	
@@ -179,19 +245,65 @@
 		font-weight: 300;
 		font-variant-numeric: oldstyle-nums;
 	}
+	.bubble {
+		cursor: pointer;
+		transition: .3s opacity ease;
+	}
+	.bubble:hover circle.size  {
+		fill: var(--bgColor);
+	}
+	
+	.bubble.hide {
+		opacity: 0;
+		pointer-events: none;
+	}
+	
+	.line-path {
+		fill: none;
+		stroke: var(--borderColor);
+		stroke-width: 4px;
+	}
+	.line-label {
+		font-size: .9rem;
+		color: var(--textColor);
+		font-weight: 500;
+	}
 </style>
 
-
-<g transform="translate({$pos.x}, {$pos.y})" opacity={$opacity} style="
+<g
+	style="
 	--borderColor: {borderColor};
 	--baseSizeStrokeColor: {baseSizeStrokeColor};
 	--textColor: {textColor};
 	--bgColor: {bgColor};
-">
-	<text y={textPos.y} x={textPos.x} class="pollutant--name" text-anchor="middle">{pollutant.name}</text>
+	--bgColorTransparent: {bgColorTransparent}"
+>
+
+{#if isActive}
+
+<path d={linePath} class="line-path" />
+{#each $linePathTweened.filter(([filterYear, _]) => filterYear <= year) as point}
+	{#if linePathLabels[pollutant.name].includes(parseInt(point[0]))}
+		<text class="line-label" x={point[1][0]} y={point[1][1]} transform="translate(0, 20)" text-anchor="middle">{point[0]}</text>
+		
+	{/if}
+{/each}
+
+{/if}
+
+<g 
+class="bubble" 
+class:active={isActive}
+class:hide={!isActive && someActive}
+transform="translate({$pos.x}, {$pos.y})" opacity={$opacity}
+bind:this={element}
+on:click={() => activate()}
+>
 	<circle cx="0" cy="0" r="{$size}" class="size"></circle>
 	<circle cx="0" cy="0" r="{scales.size(baseValue)}" stroke-dasharray="5,10" on:click={clickEvent} class="base-size"></circle>
+	<text y={textPos.y} x={textPos.x} class="pollutant--name" text-anchor="middle">{pollutant.name}</text>
 	<text y="{$size + 20}" x="" class="pollutant--percentage" text-anchor="middle">
 		{getPercantageString(percentageChangeToBase)} %
 	</text>
+</g>
 </g>
